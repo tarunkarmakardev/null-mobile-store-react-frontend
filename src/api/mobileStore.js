9579a783a605fc69f37c0b9d40e583779d831948
Tarun <tarun.karmakar.dev@gmail.com>
2021-05-20 11:23:00 +0530

    changes to backend url

--- src/api/mobileStore.js
+++ src/api/mobileStore.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
